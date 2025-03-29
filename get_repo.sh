40aa17cca7ed389b7518a1125726ec5540807e7c
Andrew Pareles <andrewpareles@gmail.com>
2025-03-28 22:17:59 -0700

    remove huge unused part of get_repo.sh

--- get_repo.sh
+++ get_repo.sh
@@ -22,60 +22,12 @@ if [[ "${CI_BUILD}" != "no" ]]; then
   git config --global --add safe.directory "/__w/$( echo "${GITHUB_REPOSITORY}" | awk '{print tolower($0)}' )"
 fi
 
-if [[ -z "${RELEASE_VERSION}" ]]; then
-  if [[ "${VSCODE_LATEST}" == "yes" ]] || [[ ! -f "${VSCODE_QUALITY}.json" ]]; then
-    echo "Retrieve lastest version"
-    UPDATE_INFO=$( curl --silent --fail "https://update.code.visualstudio.com/api/update/darwin/${VSCODE_QUALITY}/0000000000000000000000000000000000000000" )
-  else
-    echo "Get version from ${VSCODE_QUALITY}.json"
-    MS_COMMIT=$( jq -r '.commit' "${VSCODE_QUALITY}.json" )
-    MS_TAG=$( jq -r '.tag' "${VSCODE_QUALITY}.json" )
-  fi
+echo "Get version from ${VSCODE_QUALITY}.json"
+MS_COMMIT=$( jq -r '.commit' "${VSCODE_QUALITY}.json" )
+MS_TAG=$( jq -r '.tag' "${VSCODE_QUALITY}.json" )
 
-  if [[ -z "${MS_COMMIT}" ]]; then
-    echo "Use the latest commit from the main branch"
-    MS_COMMIT="main"
-    MS_TAG=$( echo "${UPDATE_INFO}" | jq -r '.name' )
-
-    if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
-      MS_TAG="${MS_TAG/\-insider/}"
-    fi
-  fi
-
-  date=$( date +%Y%j )
-
-  if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
-    RELEASE_VERSION="${MS_TAG}.${date: -5}-insider"
-  else
-    RELEASE_VERSION="${MS_TAG}.${date: -5}"
-  fi
-else
-  if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
-    if [[ "${RELEASE_VERSION}" =~ ^([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+-insider$ ]];
-    then
-      MS_TAG="${BASH_REMATCH[1]}"
-    else
-      echo "Error: Bad RELEASE_VERSION: ${RELEASE_VERSION}"
-      exit 1
-    fi
-  else
-    if [[ "${RELEASE_VERSION}" =~ ^([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+$ ]];
-    then
-      MS_TAG="${BASH_REMATCH[1]}"
-    else
-      echo "Error: Bad RELEASE_VERSION: ${RELEASE_VERSION}"
-      exit 1
-    fi
-  fi
-
-  if [[ "${MS_TAG}" == "$( jq -r '.tag' "${VSCODE_QUALITY}".json )" ]]; then
-    MS_COMMIT=$( jq -r '.commit' "${VSCODE_QUALITY}".json )
-  else
-    echo "Error: No MS_COMMIT for ${RELEASE_VERSION}"
-    exit 1
-  fi
-fi
-
+date=$( date +%Y%j )
+RELEASE_VERSION="${MS_TAG}.${date: -5}"
 echo "RELEASE_VERSION=\"${RELEASE_VERSION}\""
 
 mkdir -p vscode
@@ -84,27 +36,6 @@ cd vscode || { echo "'vscode' dir not found"; exit 1; }
 git init -q
 git remote add origin https://github.com/voideditor/void.git
 
-# figure out latest tag by calling MS update API
-if [[ -z "${MS_TAG}" ]]; then
-  echo "CALLING LATEST"
-  UPDATE_INFO=$( curl --silent --fail "https://update.code.visualstudio.com/api/update/darwin/${VSCODE_QUALITY}/0000000000000000000000000000000000000000" )
-  MS_COMMIT=$( echo "${UPDATE_INFO}" | jq -r '.version' )
-  MS_TAG=$( echo "${UPDATE_INFO}" | jq -r '.name' )
-elif [[ -z "${MS_COMMIT}" ]]; then
-  echo "-z MS_COMMIT!!! "
-  REFERENCE=$( git ls-remote --tags | grep -x ".*refs\/tags\/${MS_TAG}" | head -1 )
-
-  if [[ -z "${REFERENCE}" ]]; then
-    echo "Error: The following tag can't be found: ${MS_TAG}"
-    exit 1
-  elif [[ "${REFERENCE}" =~ ^([[:alnum:]]+)[[:space:]]+refs\/tags\/([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
-    MS_COMMIT="${BASH_REMATCH[1]}"
-    MS_TAG="${BASH_REMATCH[2]}"
-  else
-    echo "Error: The following reference can't be parsed: ${REFERENCE}"
-    exit 1
-  fi
-fi
 
 echo "MS_COMMIT=\"${MS_COMMIT}\""
 echo "MS_TAG=\"${MS_TAG}\""
