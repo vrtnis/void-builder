608318fa80b0050855f7bdc1c4968c079c5f0635
vrtnis <123119434+vrtnis@users.noreply.github.com>
2025-05-24 03:38:17 +0000

    changed source to vrtnis fork

--- get_repo.sh
+++ get_repo.sh
@@ -22,14 +22,14 @@ if [[ "${CI_BUILD}" != "no" ]]; then
   git config --global --add safe.directory "/__w/$( echo "${GITHUB_REPOSITORY}" | awk '{print tolower($0)}' )"
 fi
 
-VOID_BRANCH="main"
+VOID_BRANCH="windows-arm64-build"
 echo "Cloning void ${VOID_BRANCH}..."
 
 mkdir -p vscode
 cd vscode || { echo "'vscode' dir not found"; exit 1; }
 
 git init -q
-git remote add origin https://github.com/voideditor/void.git
+git remote add origin https://github.com/vrtnis/void.git
 
 # Allow callers to specify a particular commit to checkout via the
 # environment variable VOID_COMMIT.  We still default to the tip of the
